82bdba90c531e7dc16e4f25ecd19cb857a48602c
romans1212notes <romans1212notes>
2017-07-15 23:42:46 -0700

    Add convert_csv_to_mnist.py, read_data()

---
+++ stk/convert_csv_to_mnist.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+def read_data(csv_filename):
+    csv = np.genfromtxt(csv_filename, delimiter=",")
+    print csv.dtype
+    print csv.shape
+    
+    # useful data, remove the header, and the first column
+    data = csv[1:,1:]
+    return data
+
+
+def create_training(data, day_len):
+    num_samples = data.shape[0] - day_len  # need one more sample for label.
+    images = np.arange(0, dtype=np.float64)
+    labels = np.arange(0, dtype=np.float64)
+    for i in xrange(num_samples):
+        sample = data[i:i+day_len,:]
+	close_delta = data[i+day_len, 3] - data[i+day_len-1, 3]
+	label = 1 if close_delta > 0 else 0
+	images = np.append(images, sample)
+	labels = np.append(labels, label)
+    return (images, labels)
+
+
+csv_filename = './600000.ss.txt'
+data = read_data(csv_filename)
+
+images, labels = create_training(data, 2)
+print('images')
+print images.shape
+print images.ndim
+print images[0:48]
+
+print('labels')
+print labels.shape
+print labels.ndim
+print labels[:10]
+
+
+
+        
+    
+
+
+
+
+
