1d77f2a9943c039524408384040da6cf260d8968
romans1212notes <romans1212notes>
2017-07-16 23:31:03 -0700

    Change label to uint8

--- stk/convert_csv_to_mnist.py
+++ stk/convert_csv_to_mnist.py
@@ -13,8 +13,9 @@ def read_data(csv_filename):
 
 def create_training(data, day_len):
     num_samples = data.shape[0] - day_len  # need one more sample for label.
+    # contrib/learn/python/learn/datasets/mnist.py. images are is unint8, 
     images = np.arange(0, dtype=np.float64)
-    labels = np.arange(0, dtype=np.float64)
+    labels = np.arange(0, dtype=np.uint8)
     for i in xrange(num_samples):
         sample = data[i:i+day_len,:]
 	close_delta = data[i+day_len, 3] - data[i+day_len-1, 3]
@@ -24,18 +25,21 @@ def create_training(data, day_len):
     return (images, labels)
 
 
+#def write_training(images, labels):
+    
+
 csv_filename = './600000.ss.txt'
 data = read_data(csv_filename)
 
 images, labels = create_training(data, 2)
 print('images')
-print images.shape
-print images.ndim
+print 'images.shape:', images.shape
+print 'images.ndim:', images.ndim
 print images[0:48]
 
 print('labels')
-print labels.shape
-print labels.ndim
+print 'labels.shape:', labels.shape
+print 'labels.ndim:', labels.ndim
 print labels[:10]
 
 
