00f9a6bdcc434ae8f6dcac35db5ff85898d20a40
romans1212notes <romans1212notes>
2017-07-17 23:31:49 -0700

    Fix parameters for deep learning

--- stk/mnist_deep.py
+++ stk/mnist_deep.py
@@ -55,7 +55,7 @@ def deepnn(x):
   # Last dimension is for "features" - there is only one here, since images are
   # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
   with tf.name_scope('reshape'):
-    x_image = tf.reshape(x, [-1, 28, 28, 1])
+    x_image = tf.reshape(x, [-1, 6, 8, 1])
 
   # First convolutional layer - maps one grayscale image to 32 feature maps.
   with tf.name_scope('conv1'):
@@ -68,22 +68,22 @@ def deepnn(x):
     h_pool1 = max_pool_2x2(h_conv1)
 
   # Second convolutional layer -- maps 32 feature maps to 64.
-  with tf.name_scope('conv2'):
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-
-  # Second pooling layer.
-  with tf.name_scope('pool2'):
-    h_pool2 = max_pool_2x2(h_conv2)
+#  with tf.name_scope('conv2'):
+#    W_conv2 = weight_variable([5, 5, 32, 64])
+#    b_conv2 = bias_variable([64])
+#    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+#
+#  # Second pooling layer.
+#  with tf.name_scope('pool2'):
+#    h_pool2 = max_pool_2x2(h_conv2)
 
   # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
   # is down to 7x7x64 feature maps -- maps this to 1024 features.
   with tf.name_scope('fc1'):
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    W_fc1 = weight_variable([3 * 4 * 32, 1024])
     b_fc1 = bias_variable([1024])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_pool2_flat = tf.reshape(h_pool1, [-1, 3*4*32])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
   # Dropout - controls the complexity of the model, prevents co-adaptation of
@@ -94,8 +94,8 @@ def deepnn(x):
 
   # Map the 1024 features to 10 classes, one for each digit
   with tf.name_scope('fc2'):
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
+    W_fc2 = weight_variable([1024, 2])
+    b_fc2 = bias_variable([2])
 
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
   return y_conv, keep_prob
@@ -127,7 +127,7 @@ def bias_variable(shape):
 def main(_):
   # Import data
   dim_per_day = 6
-  day_len = 4 # 
+  day_len = 8 #
   dim = day_len * dim_per_day
   num_classes = 2
   # Import data
