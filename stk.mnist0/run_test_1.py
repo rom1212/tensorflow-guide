06bb69506998909cd5c999a8722ae2602bcbd2bd
romans1212notes <romans1212notes>
2017-08-05 23:07:38 -0700

    Iterate 20 times for test, and get average results

--- stk.mnist0/run_test_1.py
+++ stk.mnist0/run_test_1.py
@@ -12,8 +12,21 @@ if __name__ == '__main__':
   FLAGS, unparsed = parser.parse_known_args()
   print('FLAGS:', FLAGS, ', unparsed:', unparsed)
 
+  print('####################### test mnist_softmax.py #######################')
   mnist_softmax.FLAGS = FLAGS
-  accuracy, precision = mnist_softmax.main([sys.argv[0]] + unparsed)
-  print('accuracy: {}, precision: {}'.format(accuracy, precision))
+  iter = 20
+  accu_list = []
+  prec_list = []
+  for i in range(iter):
+    print('################## iter: %d #################' % i)
+    accuracy, precision = mnist_softmax.main([sys.argv[0]] + unparsed)
+    accu_list.append(accuracy)
+    prec_list.append(precision)
+    print('accuracy: {}, precision: {}'.format(accuracy, precision))
+
+  print('accu_list:', accu_list)
+  print('prec_list:', prec_list)
+  print('average accuracy :', sum(accu_list)/len(accu_list))
+  print('average precision:', sum(prec_list)/len(prec_list))
 
   # tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
